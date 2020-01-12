4b2897a3254c13973a98d19201d1081d12a77624
johnson.zhang <johnson.zhang@litex.io>
2020-01-12 10:56:25 +0800

    add interceptor for logging request info

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppInterceptor } from './interceptor/app.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalInterceptors(new AppInterceptor())
   await app.listen(3000);
 }
 bootstrap();
